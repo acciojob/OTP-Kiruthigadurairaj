963cc45a215ada68b97a2adad324127813f5cae0
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-27 18:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+  document.addEventListener("DOMContentLoaded", () => {
+            const inp = document.querySelectorAll('.code');
+
+            inp.forEach((txt, index) => {
+                txt.addEventListener('input', (e) => {
+                    if (e.target.value.length === 1 && index < inp.length - 1) {
+                        inp[index + 1].focus();
+                    }
+                });
+
+                txt.addEventListener('keydown', (e) => {
+                    if (e.key === "Backspace" && index > 0 && e.target.value === '') {
+                        inp[index - 1].focus();
+                    }
+                });
+            });
+        });
